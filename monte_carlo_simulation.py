10c1bc138730c0f94287d5e35e35f08a21b55253
Michael Sands <michaelesands@gmail.com>
2023-01-03 02:22:03 -0500

    refactor

--- monte_carlo_simulation.py
+++ monte_carlo_simulation.py
@@ -43,6 +43,7 @@ def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
         if k[0] == 0 or k[1] < header_columns:
             cell.set_text_props(weight='bold', color='w')
             cell.set_facecolor(header_color)
+        
         else:
             cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
     return ax.get_figure(), ax
